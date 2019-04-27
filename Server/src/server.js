0dcc4efb7325331c21cbdca90415af3f1fb69b75
Fatih Ertuğral <fatih.ertugral@outlook.com>
2019-04-27 14:53:51 +0300

    environment variables implementing

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,16 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/routes');
-
 const app = express();
+
+const result = dotenv.config({path: './src/config/.env'});
+if (result.error) {
+  throw result.error
+}
+//console.log(result.parsed);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,5 +18,5 @@ app.use(cookieParser());
 
 app.get('/',(req, res) => res.send('hello world'));
 app.use(router);
-app.listen(3000);
+app.listen(process.env.PORT);
 module.exports = app;
