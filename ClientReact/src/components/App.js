8c95a052b32d74a384e0f158d2d536f68328a416
Fatih Ertuğral <fatih.ertugral@outlook.com>
2019-04-29 19:13:02 +0300

    Üyelik işlemleri için basit rotalar eklendi

--- ClientReact/src/components/App.js
+++ ClientReact/src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import Home from '../components/pages/Home';
+import RegisterForm  from '../components/pages/RegisterForm';
+import LoginForm  from '../components/pages/LoginForm';
 import '../css/App.css';
+import '../css/bootstrap-grid.min.css';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/register" component={RegisterForm} />
   </Switch>
 );
 
--- ClientReact/src/components/pages/Home.js
+++ ClientReact/src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../css/Home.css';
 
 class Home extends React.Component {
@@ -7,7 +8,12 @@ class Home extends React.Component {
       <div className="Home">
         <div className="Home-header">
           <h2>Merhaba Dünya!</h2>
+
         </div>
+        <ul>
+            <li><Link to='/login'>Oturum aç</Link></li>
+            <li><Link to='/register'>Kayıt ol</Link></li>
+          </ul>
       </div>
     );
   }
