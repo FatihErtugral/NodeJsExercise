adf0c6a7a9565725015dd1e27d06c339389620ce
Fatih Ertuğral <fatih.ertugral@outlook.com>
2019-05-14 22:05:19 +0300

    GraphQL Client implementasyonu

--- ClientReact/src/client.js
+++ ClientReact/src/client.js
@@ -3,10 +3,47 @@ import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { hydrate } from 'react-dom';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:3005/graphql',
+    headers: {
+      //authorization: localStorage.setItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+  // resolvers,
+  // typeDefs,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+
 hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
