68f48f5f0d634bac1f85437e1215acd5684432cb
Fatih Ertuğral <fatih.ertugral@outlook.com>
2019-05-14 10:57:14 +0300

    Login  -> GraphQL implementasyonu

--- Server/src/graphql/resolvers.js
+++ Server/src/graphql/resolvers.js
@@ -2,7 +2,8 @@ const { mergeResolvers } = require('../helpers/graphql/merge-object');
 const user = require('./user/user.resolvers');
 const role = require('./role/role.resolvers');
 const chat = require('./chat/chat.resolvers');
+const login = require('./login/login.resolvers');
 
-const resolvers = mergeResolvers(user, role, chat);
+const resolvers = mergeResolvers(user, role, chat, login);
 
 module.exports = resolvers;
